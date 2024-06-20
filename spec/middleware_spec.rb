b77769af76ed46d3cbbb94061c1a66ff6d4dde4f
Matt Jankowski <matt@jankowski.online>
2024-06-20 10:09:50 -0400

    Formatting once over on spec

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -1,24 +1,18 @@
-require 'http_accept_language'
-require 'rack/test'
-require 'json'
-
 class TestRackApp
-
   def call(env)
     request = Rack::Request.new(env)
     http_accept_language = env.http_accept_language
     result = {
-      :user_preferred_languages => http_accept_language.user_preferred_languages,
+      user_preferred_languages: http_accept_language.user_preferred_languages
     }
-    if request.params['preferred']
-      result[:preferred_language_from] = http_accept_language.preferred_language_from(request.params['preferred'])
+    if request.params["preferred"]
+      result[:preferred_language_from] = http_accept_language.preferred_language_from(request.params["preferred"])
     end
-    [ 200, {}, [ JSON.generate(result) ]]
+    [200, {}, [JSON.generate(result)]]
   end
-
 end
 
-describe "Rack integration" do
+RSpec.describe "Rack integration" do
   include Rack::Test::Methods
 
   def app
@@ -29,32 +23,31 @@ describe "Rack integration" do
   end
 
   it "handles reuse of the env instance" do
-    env = { "HTTP_ACCEPT_LANGUAGE" => "en" }
+    env = {"HTTP_ACCEPT_LANGUAGE" => "en"}
     app = lambda { |env| env }
     middleware = HttpAcceptLanguage::Middleware.new(app)
     middleware.call(env)
-    expect(env.http_accept_language.user_preferred_languages).to eq %w{en}
+    expect(env.http_accept_language.user_preferred_languages).to eq %w[en]
     env["HTTP_ACCEPT_LANGUAGE"] = "de"
     middleware.call(env)
-    expect(env.http_accept_language.user_preferred_languages).to eq %w{de}
+    expect(env.http_accept_language.user_preferred_languages).to eq %w[de]
   end
 
   it "decodes the HTTP_ACCEPT_LANGUAGE header" do
-    request_with_header 'en-us,en-gb;q=0.8,en;q=0.6,es-419'
-    expect(r['user_preferred_languages']).to eq %w{en-US es-419 en-GB en}
+    request_with_header "en-us,en-gb;q=0.8,en;q=0.6,es-419"
+    expect(body_as_json["user_preferred_languages"]).to eq %w[en-US es-419 en-GB en]
   end
 
   it "finds the first available language" do
-    request_with_header 'en-us,en-gb;q=0.8,en;q=0.6,es-419', :preferred => %w(en en-GB)
-    expect(r['preferred_language_from']).to eq 'en-GB'
+    request_with_header "en-us,en-gb;q=0.8,en;q=0.6,es-419", preferred: %w[en en-GB]
+    expect(body_as_json["preferred_language_from"]).to eq "en-GB"
   end
 
   def request_with_header(header, params = {})
-    get "/", params, 'HTTP_ACCEPT_LANGUAGE' => header
+    get "/", params, "HTTP_ACCEPT_LANGUAGE" => header
   end
 
-  def r
+  def body_as_json
     JSON.parse(last_response.body)
   end
-
 end
